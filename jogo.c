6aae38744453b7f097b91fb7f8c2d3058630b9b9
CaioQC <caioq2004@gmail.com>
2023-08-17 13:55:30 -0300

    Adicionando jogo.c

---
+++ jogo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Bem vindo ao nosso jogo da advinhação\n");
+
+    int numerosecreto  = 42;
+
+    int chute;
+
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d\n", chute);
+
+    int acertou = (chute == numerosecreto);
+
+    if(acertou){
+        printf("Parabéns você acertou!\n");
+        printf("Você é um bom jogador, jogue novamente!");
+    }
+
+    else{
+        if(chute > numerosecreto){
+            printf("Seu chute foi maior que o numero secreto.\n");
+        }
+
+        if(chute < numerosecreto){
+            printf("Seu chute foi menor que o numero secreto.\n");
+        }
+
+        printf(" Você errou, tente novamente!");
+    }
+
+    return 0;
+}
