3ab318414320032c4eab0f3965bbca663fb0cf2e
Caio <caioq2004@gmail.com.br>
2024-05-29 15:47:48 -0300

    Adiconando mudanças da imagem, busca e admin do projeto Alura-space

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from galeria.views import index, imagem
+from galeria.views import index, imagem, buscar
 
 urlpatterns = [
     path("", index, name = "index"),
-    path("imagem/", imagem, name = "imagem")
+    path("imagem/<int:foto_id>", imagem, name = "imagem"),
+    path("buscar", buscar, name = "buscar")
 ]
