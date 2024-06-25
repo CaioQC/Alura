16e570fe2a794fcd1bafb9a55c29d2e7f32d4f33
Caio <caioq2004@gmail.com.br>
2024-06-25 13:54:21 -0300

    Organizando diretórios

---
+++ apps/galeria/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from galeria.views import index, imagem, buscar
+
+urlpatterns = [
+    path("", index, name = "index"),
+    path("imagem/<int:foto_id>", imagem, name = "imagem"),
+    path("buscar", buscar, name = "buscar")
+]
